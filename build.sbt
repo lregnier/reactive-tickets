c5a2f871d6cad22a1d5c1d242f8f40b4fd1e627d
lregnier <leonardoregnier@gmail.com>
2017-07-24 22:14:34 -0300

    Adding Persistence features.

--- build.sbt
+++ build.sbt
@@ -9,10 +9,15 @@ libraryDependencies ++= {
   val akkaHttpVersion = "10.0.7"
   val akkaHttpJson4sVersion = "1.11.0"
   val json4sVersion = "3.5.0"
+  val levelDbVersion = "0.7"
+  val levelDbJniVersion = "1.8"
   Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
+    "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
+    "org.iq80.leveldb" % "leveldb" % levelDbVersion,
+    "org.fusesource.leveldbjni" % "leveldbjni-all" % levelDbJniVersion,
     "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
     "de.heikoseeberger" %% "akka-http-json4s" % akkaHttpJson4sVersion,
--- src/main/scala/services/TicketSeller.scala
+++ src/main/scala/services/TicketSeller.scala
@@ -1,10 +1,12 @@
 package services
 
-import akka.actor.{Actor, FSM, Props}
+import akka.actor.{Actor, Props}
 import akka.cluster.sharding.ShardRegion
+import akka.persistence.fsm.PersistentFSM
+import akka.persistence.fsm.PersistentFSM.FSMState
 import domain.{Ticket, _}
 
-import scala.collection.{immutable, mutable}
+import scala.reflect.{ClassTag, classTag}
 
 object TicketSeller {
   val Name = "ticket-seller"
@@ -14,26 +16,44 @@ object TicketSeller {
   }
 
   // States
-  sealed trait State
-  case object Idle extends State
-  case object Active extends State
-  case object SoldOut extends State
+  sealed trait State extends FSMState
+
+  case object Idle extends State {
+    override def identifier: String = "idle"
+  }
+
+  case object Active extends State {
+    override def identifier: String = "active"
+  }
+  case object SoldOut extends State {
+    override def identifier: String = "sold-out"
+  }
 
   // Data
-  sealed trait Data
-  case object EmptyData extends Data
-
-  class BoxOffice(addTickets: immutable.Set[Ticket]) extends Data {
-    private val internalTickets = mutable.Set(addTickets.toSeq:_*)
-
-    def buy(): Option[Ticket] = {
-      val ticket = internalTickets.headOption
-      ticket.foreach(t => internalTickets.remove(t))
-      ticket
-    }
-
-    def tickets(): Seq[Ticket] = internalTickets.toSeq
+  sealed trait BoxOffice {
+    def buy(): (Option[Ticket], BoxOffice)
+    def tickets(): Seq[Ticket]
   }
+
+  case object EmptyBoxOffice extends BoxOffice {
+    def buy(): (Option[Ticket], BoxOffice) = (None, EmptyBoxOffice)
+    def tickets(): Seq[Ticket] = Seq.empty
+  }
+
+  case class NonEmptyBoxOffice(tickets: Seq[Ticket]) extends BoxOffice {
+    def buy(): (Option[Ticket], BoxOffice) = {
+      tickets match {
+        case ticket :: Nil => (Some(ticket), EmptyBoxOffice)
+        case ticket :: rest => (Some(ticket), NonEmptyBoxOffice(rest))
+      }
+    }
+  }
+
+  // Events
+  sealed trait DomainEvent
+  case class TicketsAdded(tickets: Seq[Ticket]) extends DomainEvent
+  case object TicketBought extends DomainEvent
+
 
   object Sharding {
     val extractEntityId: ShardRegion.ExtractEntityId = {
@@ -47,38 +67,62 @@ object TicketSeller {
 
 }
 
-class TicketSeller extends Actor with FSM[TicketSeller.State, TicketSeller.Data] {
+class TicketSeller extends Actor with PersistentFSM[TicketSeller.State, TicketSeller.BoxOffice, TicketSeller.DomainEvent] {
 
   import TicketSeller._
 
-  startWith(Idle, EmptyData)
+  override def persistenceId: String = {
+    // Note:
+    // self.path.parent.parent.name is the ShardRegion actor name: job-supervisor
+    // self.path.parent.name is the Shard supervisor actor name: 5
+    // self.path.name is the sharded Entity actor name: 75430231-b920-4811-8950-dad9ca18f3a8
+    s"${self.path.parent.parent.name}-${self.path.parent.name}-${self.path.name}"
+  }
+
+  override def domainEventClassTag: ClassTag[DomainEvent] = classTag[DomainEvent]
+
+  override def applyEvent(domainEvent: DomainEvent, boxOfficeBeforeEvent: BoxOffice): BoxOffice = {
+    domainEvent match {
+      case TicketsAdded(tickets) => NonEmptyBoxOffice(tickets)
+
+      case TicketBought =>
+        val (_, newBoxOffice) = boxOfficeBeforeEvent.buy()
+        newBoxOffice
+    }
+  }
+
+  startWith(Idle, EmptyBoxOffice)
 
   when(Idle) {
     case Event(EventMessage(_, AddTickets(tickets)), _) => {
-      goto(Active) using new BoxOffice(tickets)
+      goto(Active) applying TicketsAdded(tickets.toSeq)
     }
   }
 
   when(Active) {
-    case Event(EventMessage(_, BuyTicket), boxOffice: TicketSeller.BoxOffice) => {
-      sender ! boxOffice.buy()
-      if (boxOffice.tickets.nonEmpty) stay using boxOffice
-      else goto(SoldOut) using boxOffice
+    case Event(EventMessage(_, BuyTicket), boxOffice: BoxOffice) => {
+      val (boughtTicket, newBoxOffice) = boxOffice.buy()
+      sender ! boughtTicket
+      newBoxOffice match {
+        case _: NonEmptyBoxOffice => stay applying TicketBought
+        case EmptyBoxOffice => goto(SoldOut) applying TicketBought
+      }
     }
 
-    case Event(EventMessage(_, ListTickets), boxOffice: TicketSeller.BoxOffice) => {
+    case Event(EventMessage(_, ListTickets), boxOffice: BoxOffice) => {
       sender ! boxOffice.tickets
       stay
     }
   }
 
   when(SoldOut) {
-    case Event(EventMessage(_, BuyTicket), boxOffice: TicketSeller.BoxOffice) => {
-      sender ! boxOffice.buy()
+    case Event(EventMessage(_, BuyTicket), boxOffice: BoxOffice) => {
+      val (boughtTicket, _) = boxOffice.buy()
+      sender ! boughtTicket
       stay
     }
 
-    case Event(EventMessage(_, ListTickets), boxOffice: TicketSeller.BoxOffice) => {
+    case Event(EventMessage(_, ListTickets), boxOffice: BoxOffice) => {
       sender ! boxOffice.tickets
       stay
     }
@@ -95,7 +139,5 @@ class TicketSeller extends Actor with FSM[TicketSeller.State, TicketSeller.Data]
       stay
     }
   }
-
-  initialize()
 
 }
